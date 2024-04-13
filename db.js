d97cb88fa39335495734543e3ac0ea70b3906acb
rahul <rajrahul2022@gmail.com>
2024-04-13 18:24:18 +0530

    second version of our app

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 // Define the MongoDB connection URL
-// const mongoURL= process.env.MONGODB_URL_LOCAL // Replace 'hotels' with your desired database name
+const mongoURL= process.env.MONGODB_URL_LOCAL // Replace 'hotels' with your desired database name
 const mongoURL = process.env.MONGODB_URL;
 
 // Set up MongoDB connection
